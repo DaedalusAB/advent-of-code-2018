9d65ff403703b70b888f71319db02ec4e2f201ac
Aleksandar Budić <aleksandar.budic@nultien.rs>
2018-12-30 14:19:23 +0100

    day 3 half solved

---
+++ Challenge3/FabricPiece.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge3
+{
+    public class FabricPiece : ValueObject
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public FabricPiece(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
