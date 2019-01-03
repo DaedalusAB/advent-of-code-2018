f2640d1a794afe03c51b21d5bc4cbb65236696d9
Aleksandar Budić <aleksandar.budic@nultien.rs>
2019-01-03 09:18:44 +0100

    small touches/refactor

--- Challenge3/FabricTracker.cs
+++ Challenge3/FabricTracker.cs
@@ -8,13 +8,11 @@ namespace Challenge3
         private const int Size = 1000;
         private readonly List<FabricClaim> _trackedClaims;
         private readonly FabricPiece[] _pieces;
-        private readonly Dictionary<int, bool> _overlapMap;
 
         public FabricTracker()
         {
             _trackedClaims = new List<FabricClaim>();
             _pieces = new FabricPiece[Size * Size];
-            _overlapMap = new Dictionary<int, bool>();
         }
 
         public int TotalOverlap =>
@@ -22,7 +20,16 @@ namespace Challenge3
                 .Where(p => p != null)
                 .Count(p => p.Claims.Count > 1);
 
-
+        public int SingleNonOverlappingClaim =>
+            // ewwwwwww
+            _pieces
+                .Where(p => p?.Claims.Count == 1)
+                .Select(p => p.Claims.First())
+                .GroupBy(p => p)
+                .Select(g => new { Id = g.Key, Size = g.Count() })
+                .First(g => g.Size == _trackedClaims.Find(p => p.Id == g.Id).Size)
+                .Id;
+        
         public void TrackAll(IEnumerable<FabricClaim> fabricClaims)
         {
             foreach (var fabricClaim in fabricClaims)
@@ -31,18 +38,10 @@ namespace Challenge3
             }
         }
 
-        public int SingleNonOverlappingClaim() =>
-            _pieces
-                .Where(p => p?.Claims.Count == 1)
-                .Select(p => p.Claims.First())
-                .GroupBy(p => p)
-                .Select(g => new {Id = g.Key, Size = g.Count()})
-                .First(g => g.Size == _trackedClaims.Find(p => p.Id == g.Id).Size)
-                .Id;
-        // ewwwwwww
-
         private void TrackSingleClaim(FabricClaim fabricClaim)
         {
+            _trackedClaims.Add(fabricClaim);
+
             foreach (var claimedPiece in fabricClaim.ClaimedCoordinates)
             {
                 var position = claimedPiece.X * Size + claimedPiece.Y;
@@ -54,8 +53,6 @@ namespace Challenge3
 
                 _pieces[position].AddClaim(fabricClaim.Id);
             }
-
-            _trackedClaims.Add(fabricClaim);
         }
     }
 }
